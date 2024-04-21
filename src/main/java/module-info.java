1841b8da6831d699f1fbf96d881487161494ee7a
kossouboly <tedji90@gmail.com>
2024-04-21 19:56:20 +0000

    project configuration

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.tedjgroup.emailclient {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    //requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.tedjgroup.emailclient to javafx.fxml;
+    exports com.tedjgroup.emailclient;
+}
